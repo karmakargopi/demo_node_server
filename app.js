da5e8ae392a2b9fcaefae25fd2202b92c406acc8
Gopi Karmakar <gopi.karmakar@gmail.com>
2018-01-22 12:29:55 +0900

    Added an error check for server initiation on port

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const controller = require('./controllers/controller');
+const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use('/', express.static('./public'));
 
 controller(app);
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || port, (err) => {
+    if(!err) {
+        console.log("Server Running At Port : ", port);
+    }
+});
